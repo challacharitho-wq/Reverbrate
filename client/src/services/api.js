e40805e566e276d6bf7746359bfb46fedc5a92e2
Charith Challa <challacharith.o@gmail.com>
2026-04-16 10:30:49 +0530

    connect frontend to render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const AUTH_TOKEN_KEY = 'reverberate_token'
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ||
+  baseURL: import.meta.env.VITE_API_URL ||
            'http://localhost:5000/api',
 })
 
